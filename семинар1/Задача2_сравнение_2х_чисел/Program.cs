ba48e10c13d2fcc7b329c30d0185e5828eae7a9c
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 21:53:41 +0300

    Сравнение чисел

---
+++ семинар1/Задача2_сравнение_2х_чисел/Program.cs
@@ -0,0 +1,17 @@
+﻿// Запросить 2 числа, и вывести является ли первое число квадратом второго!!
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int quad = numberB * numberB;
+
+if (quad == numberA)
+{
+    Console.Write("Является ");
+}
+else
+{
+    Console.Write("Не является ");
+}
