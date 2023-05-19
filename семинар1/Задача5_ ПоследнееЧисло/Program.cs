48f83e32a69fe50398d2fba54b5a99c7c47fb7c9
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 22:55:35 +0300

    Вывод последней цифры

---
+++ семинар1/Задача5_ ПоследнееЧисло/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ввести трехзначное число и вывести последнюю цифру
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+int result = numberA % 10;
+Console.WriteLine(result);
