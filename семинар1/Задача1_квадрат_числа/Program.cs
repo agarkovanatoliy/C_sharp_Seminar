fdcdca3960a60c339c4c5d1e79846c11041127be
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 21:36:11 +0300

    Вывод квадрата числа

---
+++ семинар1/Задача1_квадрат_числа/Program.cs
@@ -0,0 +1,6 @@
+﻿//Получить число от пользователя и вывести квадрат этого числа
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());   // int.Parse - команда переводит строковое значение - в числовое
+int result = number * number;
+Console.WriteLine(result);
