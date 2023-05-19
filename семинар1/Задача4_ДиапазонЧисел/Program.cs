24d71019e16f744c4080fdb1191ecbcffaa54493
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 22:48:04 +0300

    Вывод диапазона целых чисел

---
+++ семинар1/Задача4_ДиапазонЧисел/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ввести число "N" и вывести все целые числс в диапазоне то -N до +N
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int numberB = numberA * -1;
+int temp = numberA;
+
+if (numberA < 0)
+{
+    numberA = numberB;
+    numberB = temp;
+}
+while(numberB <= numberA )
+{
+     Console.Write(numberB +", ");
+     numberB ++;
+}
+   
