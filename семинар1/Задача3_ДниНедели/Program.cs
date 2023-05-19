f3c54654e7cb1a9ec386c40121a0665e389251e3
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 22:18:15 +0300

    дни недели

---
+++ семинар1/Задача3_ДниНедели/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывод дня недели по введенному числу
+
+string [] days = new string [7] {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+Console.Write("Введите число дня недели:  ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 7)
+{
+    Console.WriteLine("Неверное число!");
+}
+else if (number < 1)
+{
+    Console.WriteLine("Неверное число!");
+}
+else
+{
+    Console.WriteLine("День недели: " + days[number - 1]);
+}
