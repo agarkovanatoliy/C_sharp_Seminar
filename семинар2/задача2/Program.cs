138942e748a74070c67633966c21ea51c4ca88b9
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-25 23:19:40 +0300

    Удаление второго числа из трехзначного

--- семинар2/задача2/Program.cs
+++ семинар2/задача2/Program.cs
@@ -1,32 +1,36 @@
-﻿// найти максимум из 9 чисел с помощью функций
+﻿// Написать программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа
 
-int Max(int arg1, int arg2, int arg3)
+// int number = new Random().Next(100, 1000);
+// int firstNumber = number / 100;
+// int secondNumber = number % 10;
+// Console.WriteLine("Рандомное число: " + number);
+// Console.WriteLine( firstNumber);
+// Console.WriteLine( secondNumber);
+
+// int result = firstNumber * 10 + secondNumber;
+// Console.WriteLine("Полученное число: " + result);
+
+int FindFirstDigit( int arg) //метод определения перрвой цифры трехзначного числа
 {
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    return result; // retrn возвращает результат работы функции
+    int firstNumber = arg / 100;
+    return firstNumber;
 }
 
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 152;
-int c2 = 192;
-int a3 = 843;
-int b3 = 32;
-int c3 = 41;
+int FindSecondDigit( int arg)   //метод определения последней цифры числа
+{
+    int secondNumber = arg % 10;
+    return secondNumber;
+}
+void DoubleNumber(int arg, int arg1, int arg2)
+{
+    Console.WriteLine("Рандомное число: " + arg);
+    Console.WriteLine("Первое число: " + arg1);
+    Console.WriteLine("Третье число: " + arg2);
 
-// Первый вариант решения
+    int result = arg1 * 10 + arg2;
+    Console.WriteLine("Полученное число: " + result);
+}
 
-//int max1 = Max(a1, b1, c1);              
-//int max2 = Max(a2, b2, c2);
-//int max3 = Max(a3, b3, c3);
-//int max = Max(max1, max2, max3);
-
-// Второй вариант
-
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
-
-Console.WriteLine(max);
+int number = new Random().Next(100, 1000);
+DoubleNumber(number, FindFirstDigit(number), FindSecondDigit(number));
