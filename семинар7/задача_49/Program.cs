76fcf40230d7e174acc9d9d6254c9e03dd5599eb
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-10 10:19:38 +0300

    замена элементов стоящих на четных позициях на их квадраты

---
+++ семинар7/задача_49/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 49 Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    if ((int.TryParse(value, out int val)) == false)
+    {
+        Console.WriteLine("Это не число ");
+    }
+    return val;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int colum = 0; colum < matrix.GetLength(1); colum++)
+        {
+            Console.Write($" {matrix[row, colum]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMRandoMatrix(int rows, int colums, int from, int to)
+{
+    int[,] matrix = new int[rows, colums];
+    to++;
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int colum = 0; colum < matrix.GetLength(1); colum++)
+        {
+            matrix[row, colum] = new Random().Next(from, to);
+        }
+    }
+    return matrix;
+}
+
+void QuadElement(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0)
+        {
+            matr[i, j] = matr[i, j] * matr[i, j];
+        }
+    }
+}
+}
+
+
+int i = Prompt("Ведите количество строк массива: ");
+int j = Prompt("Введите количество столбцов массива: ");
+int from = Prompt("Введите начально значение элементов массива: ");
+int to = Prompt("Введите конечное значение элементов массива: ");
+int[,] matr = CreateMRandoMatrix(i, j, from, to);
+PrintMatrix(matr);
+QuadElement(matr);
+Console.WriteLine();
+PrintMatrix(matr);
