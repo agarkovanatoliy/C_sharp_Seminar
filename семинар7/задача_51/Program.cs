77e246fdc76f315a5959e5fd57f05270879fbe3f
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-10 10:35:06 +0300

    Сумма элементов на главной диоганали массива

---
+++ семинар7/задача_51/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 51 Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    if ((int.TryParse(value, out int val)) == false)
+    {
+        Console.WriteLine("Это не число ");
+    }
+    return val;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMRandoMatrix(int rows, int colums, int from, int to)
+{
+    int[,] matrix = new int[rows, colums];
+    to++;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(from, to);
+        }
+    }
+    return matrix;
+}
+
+int SumDioganal(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0, j = 0; i < matr.GetLength(0); i++, j++)
+    {
+        sum = sum + matr[i, j];
+
+    }
+    return sum;
+}
+
+int row = Prompt("Ведите количество строк массива: ");
+int col = Prompt("Введите количество столбцов массива: ");
+int from = Prompt("Введите начально значение элементов массива: ");
+int to = Prompt("Введите конечное значение элементов массива: ");
+int[,] matr = CreateMRandoMatrix(row, col, from, to);
+PrintMatrix(matr);
+int summ = SumDioganal(matr);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов главной диоганали равна: {summ}");
